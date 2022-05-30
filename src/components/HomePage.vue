95dccd604ae90cbb79f0c6b8959636924d7eda6c
Francisco <francisco.nunez.d@usach.cl>
2022-05-30 00:32:12 -0400

    feat: show navbar only if the user is logged

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,49 +1,43 @@
 <template>
-  <div class="HomePage">
-    <div class="PrincipalFrame">
-
-      <p class="timebank-title">Servicios populares</p>
-
-      <div class="container" style="margin: 10px auto;">
-        <mdb-carousel :interval="8000" showControls showIndicators>
-            <mdb-carousel-item>
-              <a href="Service">
-                <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).webp" alt="First slide"/>
-              </a>
-            </mdb-carousel-item>
-            <mdb-carousel-item>
-              <a href="https://youtube.com">
-                <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).webp" alt="Second slide"/>
-              </a>
-            </mdb-carousel-item>
-            <mdb-carousel-item>
-              <a href="https://twitch.tv">
-                <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).webp" alt="Third slide"/>
-              </a>
-            </mdb-carousel-item>
-        </mdb-carousel>
-      </div>
-
-      <p class="timebank-title">Categorías recomendadas</p>
-
-      <div class="container" style="margin: 10px auto;">
-        <mdb-carousel :interval="8000" showControls showIndicators>
-          <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).webp" alt="First slide" />
-          <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).webp" alt="Second slide" />
-          <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).webp" alt="Third slide" />
-        </mdb-carousel>
-      </div>
-
-      <p class="timebank-title">Últimos servicios agregados</p>
-
-      <div class="container" style="margin: 10px auto;">
-        <mdb-carousel :interval="8000" showControls showIndicators>
-          <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).webp" alt="First slide" />
-          <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).webp" alt="Second slide" />
-          <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).webp" alt="Third slide" />
-        </mdb-carousel>
-      </div>
-
+  <div class="principal-frame">
+    <!-- Popular services -->
+    <p class="timebank-title">Servicios populares</p>
+    <div class="container" style="margin: 10px auto;">
+      <mdb-carousel :interval="8000" showControls showIndicators>
+          <mdb-carousel-item>
+            <a href="Service">
+              <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).webp" alt="First slide"/>
+            </a>
+          </mdb-carousel-item>
+          <mdb-carousel-item>
+            <a href="https://youtube.com">
+              <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).webp" alt="Second slide"/>
+            </a>
+          </mdb-carousel-item>
+          <mdb-carousel-item>
+            <a href="https://twitch.tv">
+              <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).webp" alt="Third slide"/>
+            </a>
+          </mdb-carousel-item>
+      </mdb-carousel>
+    </div>
+    <!-- Recomended Categories -->
+    <p class="timebank-title">Categorías recomendadas</p>
+    <div class="container" style="margin: 10px auto;">
+      <mdb-carousel :interval="8000" showControls showIndicators>
+        <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).webp" alt="First slide" />
+        <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).webp" alt="Second slide" />
+        <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).webp" alt="Third slide" />
+      </mdb-carousel>
+    </div>
+    <!-- Last services -->
+    <p class="timebank-title">Últimos servicios agregados</p>
+    <div class="container" style="margin: 10px auto;">
+      <mdb-carousel :interval="8000" showControls showIndicators>
+        <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).webp" alt="First slide" />
+        <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).webp" alt="Second slide" />
+        <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).webp" alt="Third slide" />
+      </mdb-carousel>
     </div>
   </div>
 </template>
@@ -57,12 +51,8 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-@media (min-width: 480px) {
-  .PrincipalFrame { height: 83.5vh; }
-}
-
-.PrincipalFrame{
-  box-sizing: border-box;
+.principal-frame{
+  /* box-sizing: border-box; */
   max-width: 425px;
   margin: 0 auto;
   padding: 25px 0px;
