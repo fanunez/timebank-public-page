0ac6c125d769f58235d6c243046fef097b4ac29a
NicolasZK <nicolas.zelada@usach.cl>
2022-06-01 20:49:15 -0400

    style: change in navbar numbers and added icons to register

--- src/components/BottomNavbar.vue
+++ src/components/BottomNavbar.vue
@@ -3,9 +3,6 @@
         <!-- User logged -->
         <div v-if="userLogged" class="row" style="width:-webkit-fill-available">
             <div class="col-3" style="padding:0px;">
-              <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off" style="width: -webkit-fill-available; background-color:#74015E; box-shadow: none!important;"><Icon icon="bx:help-circle" width="20" height="20"/><br>Ayuda</button>
-            </div>
-            <div class="col-3" style="padding:0px;">
               <button type="button" onclick="location.href='/';" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off" style="width: -webkit-fill-available; background-color:#74015E; box-shadow: none!important;"><Icon icon="bxs:home" width="20" height="20"/><br>Inicio</button>
             </div>
             <div class="col-3" style="padding:0px;">
@@ -14,17 +11,20 @@
             <div class="col-3" style="padding:0px;">
               <button type="button" onclick="location.href='/profile';" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off" style="width: -webkit-fill-available; background-color:#74015E; box-shadow: none!important;"><Icon icon="bi:person-circle" width="20" height="20"/><br>Perfil</button>
             </div>
+            <div class="col-3" style="padding:0px;">
+              <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off" style="width: -webkit-fill-available; background-color:#74015E; box-shadow: none!important;"><Icon icon="bx:help-circle" width="20" height="20"/><br>Ayuda</button>
+            </div>
         </div>
           <!-- User no logged in -->
         <div v-else class="row" style="width:-webkit-fill-available">
-            <div class="col-4" style="padding:0px;">
-              <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off" style="width: -webkit-fill-available; background-color:#74015E; box-shadow: none!important;"><Icon icon="bx:help-circle" width="20" height="20"/><br>Ayuda</button>
-            </div>
             <div class="col-4" style="padding:0px;">
               <button type="button" onclick="location.href='/';" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off" style="width: -webkit-fill-available; background-color:#74015E; box-shadow: none!important;"><Icon icon="bxs:home" width="20" height="20"/><br>Inicio</button>
             </div>
             <div class="col-4" style="padding:0px;">
               <button type="button" onclick="location.href='/search-service';" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off" style="width: -webkit-fill-available; background-color:#74015E; box-shadow: none!important;"><Icon icon="charm:search" width="20" height="20"/><br>Buscar</button>
+            </div>
+            <div class="col-4" style="padding:0px;">
+              <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off" style="width: -webkit-fill-available; background-color:#74015E; box-shadow: none!important;"><Icon icon="bx:help-circle" width="20" height="20"/><br>Ayuda</button>
             </div>         
         </div>
       </nav>
--- src/components/TopNavbar2.vue
+++ src/components/TopNavbar2.vue
@@ -1,35 +1,41 @@
 <template>
     <div class="navbar-buttons fixed-top" style="padding: 10px 0px; background-color: #74015E; position:sticky;">
         <div class="row" style="margin: 10px 0px; background-color:#74015E">
-          <div class="col-3" style="padding-right: 0px;">
-              <b-dropdown no-caret toggle-class="CustomHamb" variant="none">
-              <template #button-content>
-                  <Icon icon="charm:menu-hamburger" width="28" height="32" color="white"></Icon>
-              </template>
-              <b-dropdown-item href="#">Categorias</b-dropdown-item>
-              <b-dropdown-item href="#">Añadir servicio</b-dropdown-item>
-              <b-dropdown-item href="#">Solicitar bono</b-dropdown-item>
-              <b-dropdown-item href="#">Sobre nosotros</b-dropdown-item>
-              <b-dropdown-item v-on:click="cerrarSesion">Cerrar sesión</b-dropdown-item>
-              </b-dropdown>
-          </div>
-          <div class="col-3" style="padding: 0px;">
-                <b-button class="bonos" type="button">{{this.userLoggedBono}}<Icon icon="emojione-v1:alarm-clock" style="width:32px; height:32px" /></b-button>
-          </div>
-          <div class="col-3" style="padding: 0px;">
-              <b-button class="notificaciones" type="button">1 <Icon icon="emojione-v1:ringing-bell" style="width:32px; height:32px"/></b-button>
-          </div>
-          <div class="col-3" style="padding-left: 0px; color: white;">
-              <b-dropdown right no-caret toggle-class="CustomHamb" menu-class="tuerca" variant="none">
-              <template #button-content>
-                  <Icon icon="ph:gear-duotone" width="32" height="32"/>
-              </template>
-              <b-dropdown-item href="#">+A</b-dropdown-item>
-              <b-dropdown-item href="#">-A</b-dropdown-item>
-              <b-dropdown-item href="#">+a</b-dropdown-item>
-              <b-dropdown-item href="#">-a</b-dropdown-item>
-              </b-dropdown>
-          </div>
+            <div class="col-3" style="padding-right: 0px;">
+                <b-dropdown no-caret toggle-class="CustomHamb" variant="none">
+                <template #button-content>
+                    <Icon icon="charm:menu-hamburger" width="28" height="32" color="white"></Icon>
+                </template>
+                <b-dropdown-item href="#">Categorias</b-dropdown-item>
+                <b-dropdown-item href="#">Añadir servicio</b-dropdown-item>
+                <b-dropdown-item href="#">Solicitar bono</b-dropdown-item>
+                <b-dropdown-item href="#">Sobre nosotros</b-dropdown-item>
+                <b-dropdown-item v-on:click="cerrarSesion">Cerrar sesión</b-dropdown-item>
+                </b-dropdown>
+            </div>
+            <div class="col-3" style="padding: 0px;">
+                    <b-button class="bonos" type="button">
+                        <Icon icon="akar-icons:clock" style="width:32px; height:32px"/>
+                            <span id="cart_menu_num" class="badge rounded-circle">{{this.userLoggedBono}}</span>
+                    </b-button>
+            </div>
+            <div class="col-3" style="padding: 0px;">
+                <b-button class="notificaciones" type="button">
+                    <Icon icon="akar-icons:bell" style="width:32px; height:32px"/>
+                    <span id="cart_menu_num" class="badge rounded-circle">1</span>
+                </b-button>
+            </div>
+            <div class="col-3" style="padding-left: 0px; color: white;">
+                <b-dropdown right no-caret toggle-class="CustomHamb" menu-class="tuerca" variant="none">
+                <template #button-content>
+                    <Icon icon="ph:gear-duotone" width="32" height="32"/>
+                </template>
+                <b-dropdown-item>+A</b-dropdown-item>
+                <b-dropdown-item>-A</b-dropdown-item>
+                <b-dropdown-item>+a</b-dropdown-item>
+                <b-dropdown-item>-a</b-dropdown-item>
+                </b-dropdown>
+            </div>
         </div>
     </div>
 </template>
@@ -52,8 +58,8 @@ export default {
   },
   methods: {
       cerrarSesion() {
-          auth.deleteUserLogged()
-          return window.location.href="/"
+        auth.deleteUserLogged()
+        return window.location.href="/"
       }
   }
 }
@@ -97,5 +103,20 @@ export default {
   background-color: white!important;;
   color: #74015E!important;
   border-color: #74015E!important;
+}
+#cart_menu_num {
+  position: absolute;
+  top: 0;
+  left: 55%;
+  background: red;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  padding: 2px;
+  font-size: 10px;
 }
 </style>
