1193462b71b226bfcddf9d05c310a09d77936544
NicolasZK <nicolas.zelada@usach.cl>
2022-06-08 17:55:35 -0400

    fix: button not linked to publish-service

--- src/components/user-profile/UserService.vue
+++ src/components/user-profile/UserService.vue
@@ -30,7 +30,7 @@
         </div>
       </div>
       <div class="row mb-5" style= "margin: 70px"></div>
-      <b-button type="button" class="publish-button" style="padding:0px 10px;">Publicar nuevo servicio</b-button>
+      <b-button type="button" class="publish-button" onclick="location.href='/publish-service';" style="padding:0px 10px;">Publicar nuevo servicio</b-button>
     </div>
 </template>
 
