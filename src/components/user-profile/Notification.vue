26bebc328362ff30bca03ced4c4e9ae58b4a9717
Jean Rebolledo <jean.rebolledo@usach.cl>
2022-06-26 02:16:01 -0400

    feat: notificationView

--- src/components/user-profile/Notification.vue
+++ src/components/user-profile/Notification.vue
@@ -1,21 +1,38 @@
 <template>
     <div class="notification-frame">   
-        <!--div class="row"-->
-            <div class="row justify-content-center" style="margin: 80px 35px;">
-         <div class="timebank-header mb-3" >Notificaciones</div>
-         
-         <!--div class="timebank-header" >Notificaciones</div-->
+        <div class="row " style="margin: 80px 35px;">
+            <div v-if="descNot.length==0" class= "timebank-header">
+                <div class="col-1" style="padding: 5px 0px; margin: 10px 10px;">
+                    <button onclick="history.back()" style="color:black; display: contents;"><Icon icon="akar-icons:arrow-left" style="width:40px; height:40px;"/></button>
+                </div>
+                <div class="row mb-5" style= "margin: 20px"></div>
+                Aún no has recibido notificaciones. </div>
+            <div v-else> 
+                <div class="row mb-2">
+                <div class="col-1" style="padding: 5px 0px; margin: 10px 10px;">
+                    <button onclick="history.back()" style="color:black; display: contents;"><Icon icon="akar-icons:arrow-left" style="width:40px; height:40px;"/></button>
+                </div>
+                <div class="col-8" style="padding: 0px 40px; margin-top: 5px;">
+                    <div class="timebank-header" style="margin-right: 10px">Notificaciones</div>
+                </div> <div class="col-2"></div></div>
+                <!--div class="timebank-header mb-3" >Notificaciones</div-->
+            
+            <!--div class="timebank-header" >Notificaciones</div-->
         <!--/div-->
         
-            
+                <div v-for="(notification,index) in stateNot" :key="index">
             <!--b-list-group style= "margin: 70px"-->
-               <b-list-group-item href="#" variant="dark" style="color: white; font-weight: bold;">Dark list group item</b-list-group-item>
-               <b-list-group-item href="#" variant="dark" style="color: white; font-weight: bold;">Dark list group item</b-list-group-item>
-               <b-list-group-item href="#" variant="dark" style="color: white; font-weight: bold; text-align:left;"><div class="row"><div class="col-4" style="margin: 0px -20px"><img left src="https://placekitten.com/1000/300" rounded="circle" width="60" height="60" style="margin-left: 12px; border-radius:50px;"></div><div class="col-9"><h4 text-left style= "font-weight: bold;">Esta es la notificacion con todos sus detalles</h4><h5>esta es la fecha de la notificacion</h5></div></div></b-list-group-item>
-               <b-list-group-item href="#" variant="danger" style="background-color: #A70187; color: white; font-weight: bold;">Notificacion ya leida</b-list-group-item>
-
-            <!--/b-list-group-->
-           </div>
+                <!--b-list-group-item href="#" variant="dark" style="color: white; font-weight: bold;">Dark list group item</b-list-group-item-->
+                    <div v-if="stateNot[index]==0">
+                        <b-list-group-item href="#" button style="font-weight: bold; text-align:left;"><div class="row"><div class="col-4" style="margin: 0px -20px"><img class="img-s" left :src= img rounded="circle" width="60" height="60" style="margin-left: 12px; border-radius:50px;"></div><div class="col-9"><h4 text-left style= "font-weight: bold;">{{descNot[index]}}</h4><h5>{{dateNot[index]}}</h5></div></div></b-list-group-item>
+                    </div>
+                    <div v-else>
+                        <b-list-group-item href="#" button style="background-color: #A70187; color: white; font-weight: bold; text-align:left;"><div class="row"><div class="col-4" style="margin: 0px -20px"><img class="img-s" left :src= img rounded="circle" width="60" height="60" style="margin-left: 12px; border-radius:50px;"></div><div class="col-9"><h4 text-left style= "font-weight: bold;">{{descNot[index]}}</h4><h5>{{dateNot[index]}}</h5></div></div></b-list-group-item>
+                    </div>
+                </div>
+        <!--/b-list-group-->
+            </div>
+        </div>
        
     </div>
 </template>
@@ -27,7 +44,50 @@ import auth from "@/logic/auth";
 export default {
     name: 'Notification',
     data() {
-        
+        return{
+            uid: '',
+            img: '',
+            dateNot: [],
+            descNot: [],
+            stateNot: [],
+        }
+    },
+    methods: {
+        getNotificationsUser(){
+            axios
+            .get(process.env.VUE_APP_BACKEND_URL_SERVER + '/notification/user-notifactions/' + this.uid)
+            .then( r=>{
+                console.log(this.uid);
+                console.log(r.data);
+                r.data.forEach(element => {
+                    this.dateNot.push(element.date);
+                    console.log(element.date);
+                    this.descNot.push(element.description);
+                    console.log(element.description);
+                    this.stateNot.push(element.check);
+                });
+            })
+            .catch(e => console.log(e))
+        },
+    },
+
+    async mounted() {
+        // get user uid
+        const token = auth.getUserLogged();
+        // petition
+        await axios
+            .get( process.env.VUE_APP_BACKEND_URL_SERVER + '/auth/user-logged/', {
+            headers:{
+                'Authorization': token,
+            },
+            })
+            .then( r => {
+                this.uid = r.data.uid;
+                this.img = r.data.img;
+            })
+            .catch( e => console.log( e ))
+
+            this.getNotificationsUser();
     },
 }
 </script>
